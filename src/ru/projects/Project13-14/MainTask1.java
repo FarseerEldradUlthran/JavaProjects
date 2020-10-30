c3ff3bcc429b7febd0836ee8f5ea129f9fdf49ee
FarseerEldradUlthran <66281710+FarseerEldradUlthran@users.noreply.github.com>
2020-10-30 18:00:07 +0300

    Add files via upload

--- src/ru/projects/Project13-14/MainTask1.java
+++ src/ru/projects/Project13-14/MainTask1.java
@@ -1,9 +1,21 @@
+import java.util.StringTokenizer;
+
 public class MainTask1 {
     public static void main(String[] args) {
+        int a = 0, b = 0;
+        int counter = 0;
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String[] words = text.split(" ");
-        int a = Integer.parseInt(words[2].trim());
-        int b = Integer.parseInt(words[11].trim());
+        StringTokenizer st = new StringTokenizer(text, " ");
+        while(st.hasMoreTokens()) {
+            counter++;
+            String key = st.nextToken();
+            if (counter == 3) {
+                a = Integer.parseInt(key.trim());
+            }
+            if (counter == 12) {
+                b = Integer.parseInt(key.trim());
+            }
+        }
         System.out.println("Вася и Маша в сумме получили " + (a+b) + " рублей");
     }
 }
