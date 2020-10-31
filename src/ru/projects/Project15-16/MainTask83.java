6a9cee8b4afb7a2f05bb65cd6215db29db7aa462
FarseerEldradUlthran <66281710+FarseerEldradUlthran@users.noreply.github.com>
2020-10-31 15:22:42 +0300

    Add files via upload

--- src/ru/projects/Project15-16/MainTask83.java
+++ src/ru/projects/Project15-16/MainTask83.java
@@ -5,15 +5,17 @@ import java.util.TreeMap;
 public class MainTask83 {
     public static void main(String[] args) {
         int counter = 0;
-        String input;
+        String input1;
         Map<String, String> map = new TreeMap<>();
         Scanner scanner = new Scanner(System.in);
         boolean flag = true;
         System.out.println("Доступна команда: \n LIST \n Также вы можете ввести новую информацию или найти имеющуюся ");
-        map.put("Мария", "89035487541" );
-        map.put("Алексей Петрович", "89145624514" );
+        map.put("Мария", "+7 (903) 548-75-41" );
+        map.put("Алексей Петрович", "+7 (914) 562-45-14" );
         while (flag){
-            input = scanner.nextLine();
+            input1 = scanner.nextLine();
+            String input2 = input1.replaceAll("[-!@#=$%.^&*()_+№;:?/\\\"~]*", "");
+            String input = input2.replaceAll(" ", "");
             boolean checkLetters1 = input.matches("^[a-zA-Zа-яА-Я]+$");
             boolean checkLetters2 = input.matches("[a-zA-Zа-яА-Я\\-]+[ ][a-zA-Zа-яА-Я\\-]+");
             boolean checkLetters3 = input.matches("[a-zA-Zа-яА-Я\\-]+[ ][a-zA-Zа-яА-Я\\-]+[ ][a-zA-Zа-яА-Я\\-]+");
@@ -39,12 +41,32 @@ public class MainTask83 {
                 if (counter == 0) {
                     System.out.println("Впишите номер: ");
                     Scanner scanner1 = new Scanner(System.in);
-                    String number = scanner1.next();
+                    String number1 = scanner1.nextLine();
+                    String input3 = number1.replaceAll("[-!@#=$%.^&*()_+№;:?/\\\"~]*", "");
+                    String number = input3.replaceAll(" ", "");
+                    String[] numbers = input.split("");
+                    if (numbers[0].equals("8") && numbers.length == 11 || numbers[0].equals("7") && numbers.length == 11){
+                        number = ("+7 (" + numbers[1] + numbers[2] + numbers[3] + ") " + numbers[4] +
+                                numbers[5] + numbers[6] + "-" + numbers[7] + numbers[8] + "-" + numbers[9] + numbers[10]);
+                    }
+                    else if (numbers.length == 10){
+                        number = ("+7 (" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] +
+                                numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + "-" + numbers[8] + numbers[9]);
+                    }
                     map.put(input, number);
                 }
             }
             else if (checkNumbers){
                 counter = 0;
+                String[] numbers = input.split("");
+                if (numbers[0].equals("8") && numbers.length == 11 || numbers[0].equals("7") && numbers.length == 11){
+                    input = ("+7 (" + numbers[1] + numbers[2] + numbers[3] + ") " + numbers[4] +
+                            numbers[5] + numbers[6] + "-" + numbers[7] + numbers[8] + "-" + numbers[9] + numbers[10]);
+                }
+                else if (numbers.length == 10){
+                    input = ("+7 (" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] +
+                            numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + "-" + numbers[8] + numbers[9]);
+                }
                 for(Map.Entry<String, String> item : map.entrySet()) {
                     if (input.equals(item.getValue())) {
                         System.out.println(item.getKey() + " - " + item.getValue());
